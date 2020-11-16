e88dbab728801eb2fa5daa6586c0a49be1a017c3
Saurabh Pandya <rabh@Saurabhs-MBP.lan>
2020-11-16 22:22:54 +0000

    Adding config sample file

---
+++ config/config.local.js
@@ -0,0 +1,26 @@
+const port = 1881;
+const key = "YOUR_KEY";
+
+const rooms = [];
+rooms["lounge"] = {
+  host:"192.168.1.123",
+  groups: [
+    "tv",
+    "amplifier",
+  ]
+};
+
+const commands = [
+  {
+    "command":"volume_up",
+    "group":"amplifier",
+    "data":"DATA_HERE"
+  }
+];
+
+module.exports = {
+  port,
+  key,
+  rooms,
+  commands,
+}
